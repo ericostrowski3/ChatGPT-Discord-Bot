79c695bc1b0721d8fc3d485920fd339fc79f1d3c
Eric Ostrowski <ericostrowski3@gmail.com>
2023-03-10 12:07:34 -0600

    context, handling errors, reset function

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 openai.api_key = os.getenv('OPENAI_KEY')
 # Set up Discord bot client
 bot = commands.Bot(command_prefix="$", intents=intents)
+#define messages globally for context to work
+messages = [
+    {"role": "system", "content": "You are a helpful assistant that works through Discord."}
+]
 
 # Set up OpenAI chat function
 def openai_chat(prompt):
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant that works through Discord."}
-    ]
     message=prompt
     messages.append({"role": "user", "content": message})
     
@@ -27,14 +28,26 @@ def openai_chat(prompt):
     reply = response["choices"][0]["message"]["content"]
     messages.append({"Role": "assistant", "content": reply})
     return reply
-    print(messages)
 
-# Define bot commands
+
+# Define chat command
 @bot.command()
 async def chat(ctx, *, prompt):
-    
     reply = openai_chat(prompt)
     await ctx.send(reply)
+#Define reset command 
+@bot.command()
+async def reset(ctx):
+    global messages
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant that works through Discord."}
+    ]
+    await ctx.send("Message history cleared!")
+#handle error if $chat is send without a prompt
+@chat.error
+async def chat_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Oops! You need a message after the chat command.')
 
 
 # Run the bot
