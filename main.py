deb6ea9f49231605505ef76f8c0aeb9547c84fe7
Eric Ostrowski <ericostrowski3@gmail.com>
2023-03-24 14:35:48 -0500

    technically have it working but need to modularize

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import aiosqlite
 from discord.ext import commands
 from dotenv import load_dotenv
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
+intents.message_content = True
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -15,60 +16,65 @@ messages = [
     {"role": "system", "content": "You are a helpful assistant that works through Discord."}
 ]
 
-async def create_table():
+async def create_db():
     async with aiosqlite.connect("api_keys.db") as db:
-        await db.execute("CREATE TABLE IF NOT EXISTS api_keys (user_id INTEGER PRIMARY KEY, api_key TEXT)")
+        await db.execute(
+            "CREATE TABLE IF NOT EXISTS api_keys (user_id INTEGER PRIMARY KEY, api_key TEXT)"
+        )
         await db.commit()
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is connected to Discord and ready to receive events.")
+    await create_db()
 
-async def set_user_api_key(user_id, api_key):
-    async with aiosqlite.connect("api_keys.db") as db:
-        await db.execute("INSERT OR REPLACE INTO api_keys (user_id, api_key) VALUES (?, ?)", (user_id, api_key))
-        await db.commit()
-
+# Define a function to retrieve the user's API key
 async def get_user_api_key(user_id):
     async with aiosqlite.connect("api_keys.db") as db:
         cursor = await db.execute("SELECT api_key FROM api_keys WHERE user_id=?", (user_id,))
-        result = await cursor.fetchone()
-        return result[0] if result else None
+        api_key = await cursor.fetchone()
+        return api_key[0] if api_key else None
 
-async def openai_chat(prompt, api_key):
+
+# Define a function to set/update the user's API key
+@bot.command()
+async def setapikey(ctx, *, api_key):
+    user_id = ctx.author.id
+    async with aiosqlite.connect("api_keys.db") as db:
+        await db.execute(
+            "INSERT OR REPLACE INTO api_keys (user_id, api_key) VALUES (?, ?)",
+            (user_id, api_key),
+        )
+        await db.commit()
+    await ctx.send("API key set successfully!")
+
+
+async def openai_chat(user_id, prompt):
+    api_key = await get_user_api_key(user_id)
+    if api_key is None:
+        return "you need to set your OpenAI API key first. Use the $setup command for more information"
+    openai.api_key = api_key
     message=prompt
     messages.append({"role": "user", "content": message})
-
-    openai.api_key = api_key
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
     reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
     return reply
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-    await create_table()
-
-@bot.command()
-async def setkey(ctx, api_key):
-    await set_user_api_key(ctx.author.id, api_key)
-    await ctx.author.send("Your OpenAI API key has been set!")
 
 @bot.command()
 async def chat(ctx, *, prompt):
-    api_key = await get_user_api_key(ctx.author.id)
-    
-    if api_key is None:
-        await ctx.send("Please set your OpenAI API key first using the `$setup command")
-        return
-
-    reply = await openai_chat(prompt, api_key)
+    user_id = ctx.author.id
+    async with ctx.typing():
+        reply = await openai_chat(user_id, prompt)
     await ctx.send(reply)
 
 @bot.command()
 async def setup(ctx):
     api_key = await get_user_api_key(ctx.author.id)
     if api_key is None:
-        await ctx.author.send("Please send your OpenAI API key in this format: $setkey YOUR_API_KEY")
+        await ctx.author.send("Please send your OpenAI API key in this format: $setapikey YOUR_API_KEY")
         await ctx.send("Check your messages to finish setting up your API key! :)")
     else:
         await ctx.send("You already have an API key setup. If you wish to update your key, send the bot a direct message with $setkey followed by your API key")
